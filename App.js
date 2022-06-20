70a2452988db7420a29da169d63ae79fbac52848
reuben-k-o <rubenkhaemba@gmail.com>
2022-06-20 21:34:44 +0300

    navigation

--- App.js
+++ App.js
@@ -1,12 +1,21 @@
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+
 import CategoriesScreen from "./screens/CategoriesScreen";
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
     <>
       <StatusBar style="light" />
-      <CategoriesScreen style={styles.container} />
+      <NavigationContainer>
+        <Stack.Navigator>
+          <Stack.Screen name="MealsCategory" component={CategoriesScreen} />
+        </Stack.Navigator>
+      </NavigationContainer>
     </>
   );
 }
