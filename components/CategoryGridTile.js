4f0cce0da69cf16056482bace8c7812b65339b26
reuben-k-o <rubenkhaemba@gmail.com>
2022-06-20 20:31:05 +0300

    displaying categories

---
+++ components/CategoryGridTile.js
@@ -0,0 +1,40 @@
+import { Pressable, View, Text, StyleSheet, Platform } from "react-native";
+
+function CategoryGridTile({ title, color }) {
+  return (
+    <View style={[styles.gridItem, { backgroundColor: color }]}>
+      <Pressable android_ripple={{ color: "#ccc" }} style={styles.button}>
+        <View style={styles.innerContainer}>
+          <Text style={styles.title}>{title}</Text>
+        </View>
+      </Pressable>
+    </View>
+  );
+}
+
+export default CategoryGridTile;
+
+const styles = StyleSheet.create({
+  gridItem: {
+    flex: 1,
+    margin: 16,
+    height: 150,
+    borderRadius: 8,
+    elevation: 6,
+    backgroundColor: "white",
+    overflow: Platform.OS === "android" ? "hidden" : "visible",
+  },
+  button: {
+    flex: 1,
+  },
+  innerContainer: {
+    flex: 1,
+    padding: 16,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  title: {
+    fontWeight: "bold",
+    fontSize: 18,
+  },
+});
