9686a2cfcaf3b9d2ca20c9fd5cba0e5ce6a38f4a
reuben-k-o <rubenkhaemba@gmail.com>
2022-06-23 20:41:16 +0300

    favorite logic

--- screens/FavoriteScreen.js
+++ screens/FavoriteScreen.js
@@ -1,9 +1,39 @@
-import { Text, StyleSheet } from "react-native";
+import { useContext } from "react";
+import { View, Text, StyleSheet } from "react-native";
+
+import MealsList from "../components/MealList/MealList";
+import { FavoriteContext } from "../store/context/Favorites-context";
+import { MEALS } from "../data/dummy-data";
 
 function FavoriteScreen() {
-  return <Text>Favorite screen</Text>;
+  const favoriteMealCtx = useContext(FavoriteContext);
+
+  const favoriteMeals = MEALS.filter((meal) =>
+    favoriteMealCtx.ids.includes(meal.id)
+  );
+
+  if (favoriteMeals.length === 0) {
+    return (
+      <View style={styles.rootContainer}>
+        <Text style={styles.text}> You don't have any favorite meals yet.</Text>
+      </View>
+    );
+  }
+
+  return <MealsList renderMeals={favoriteMeals} />;
 }
 
 export default FavoriteScreen;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  rootContainer: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  text: {
+    fontSize: 18,
+    fontWeight: "bold",
+    color: "white",
+  },
+});
