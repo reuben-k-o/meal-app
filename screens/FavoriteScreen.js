8e638abbc73a09950d60e9f3cc55267a75ea8435
reuben-k-o <rubenkhaemba@gmail.com>
2022-06-23 18:32:56 +0300

    favorite screen

---
+++ screens/FavoriteScreen.js
@@ -0,0 +1,9 @@
+import { View, StyleSheet } from "react-native";
+
+function FavoriteScreen() {
+  return <View>Favorite screen</View>;
+}
+
+export default FavoriteScreen;
+
+const styles = StyleSheet.create({});
